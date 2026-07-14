559e7a4a54f9956a8e78a56b0bfc37061e44554f
Daste <stefankar1000@gmail.com>
2026-07-14 21:56:09 +0200

    nix: Clean up flake

--- flake.nix
+++ flake.nix
@@ -1,13 +1,14 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    bun = {
+      url = "github:Daste745/nix-bun";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    {
-      nixpkgs,
-      ...
-    }:
+    inputs:
     let
       supportedSystems = [
         "x86_64-linux"
@@ -15,41 +16,24 @@
       ];
       eachSupportedSystem =
         f:
-        nixpkgs.lib.genAttrs supportedSystems (
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
           system:
           f {
-            inherit system;
-            pkgs = import nixpkgs { inherit system; };
+            pkgs = import inputs.nixpkgs { inherit system; };
+            bun = inputs.bun.packages.${system};
           }
         );
     in
     {
       devShells = eachSupportedSystem (
-        { system, pkgs }:
-        let
-          bunVersion = "1.2.19";
-          bunSources = {
-            "aarch64-darwin" = pkgs.fetchurl {
-              url = "https://github.com/oven-sh/bun/releases/download/bun-v${bunVersion}/bun-darwin-aarch64.zip";
-              hash = "sha256-Z0pIN4NC76rcPCkVlrVzAQ88I4iVj3xEZ42H9vt1mZE=";
-            };
-            "x86_64-linux" = pkgs.fetchurl {
-              url = "https://github.com/oven-sh/bun/releases/download/bun-v${bunVersion}/bun-linux-x64.zip";
-              hash = "sha256-w9PBTppeyD/2fQrP525DFa0G2p809Z/HsTgTeCyvH2Y=";
-            };
-          };
-          bun = pkgs.bun.overrideAttrs {
-            version = bunVersion;
-            src = bunSources.${system} or (throw "Unsupported system for bun: ${system}");
-          };
-        in
+        { pkgs, bun }:
         {
           default = pkgs.mkShell {
-            packages = with pkgs; [
-              bun
-              prisma
-              prisma-engines
-              openssl
+            packages = [
+              bun."1.2.19"
+              pkgs.prisma_6
+              pkgs.prisma-engines_6
+              pkgs.openssl
             ];
             shellHook = ''
               export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib"
